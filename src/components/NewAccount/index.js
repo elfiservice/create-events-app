5bf27df00bf4604984a60ae807150d7277a05fae
Armando Jr <elfiservice@hotmail.com>
2018-05-28 07:53:46 -0300

    feat: added form tag to NewAccount

--- src/components/NewAccount/index.js
+++ src/components/NewAccount/index.js
@@ -7,16 +7,18 @@ class NewAccount extends Component {
             <div className="new-account container">
                 <h2>Join With Us</h2>
                 <section className="content">
-                    <label for="name">
-                        <input class="input" aria-label="Name" id="name" type="text" placeholder="Name" required />
-                    </label>
-                    <label for="email">
-                        <input class="input" aria-label="Email" id="email" type="email" placeholder="Email" required />
-                    </label>
-                    <label for="pass">
-                        <input class="input" aria-label="Password" id="pass" type="password" placeholder="Password" required />
-                    </label>
-                    <button class="submit btn">Join</button>
+                    <form>
+                        <label for="name">
+                            <input class="input" autocomplete="name" aria-label="Name" id="name" type="text" placeholder="Name" required />
+                        </label>
+                        <label for="email">
+                            <input class="input" autocomplete="email" aria-label="Email" id="email" type="email" placeholder="Email" required />
+                        </label>
+                        <label for="pass">
+                            <input class="input" aria-label="Password" id="pass" type="password" placeholder="Password" required />
+                        </label>
+                        <button class="submit btn">Join</button>
+                    </form>
                 </section>
             </div>
         )
