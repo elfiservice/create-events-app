fdebb264b34f3c880093a5ee96c8d70cc4542fad
Armando Jr <elfiservice@hotmail.com>
2018-05-29 08:01:55 -0300

    refactore: relocated config of server

--- src/components/NewAccount/index.js
+++ src/components/NewAccount/index.js
@@ -3,9 +3,16 @@ import './NewAccount.css'
 import { insert } from '../../server'
 
 class NewAccount extends Component {
+
+    putUser(data) {
+        this.preventDefault();
+        console.log(data);
+        
+    }
+
     render() {
-        console.log(insert);
-        insert('11', 'junior', 'elfi@elfi.com')
+        //console.log(insert);
+        // insert('11', 'junior', 'elfi@elfi.com')
         return (
             <main className="new-account container">
                 <h2>Join With Us</h2>
@@ -14,7 +21,7 @@ class NewAccount extends Component {
                         <input className="input" autoComplete="name" aria-label="Name" id="name" type="text" placeholder="Name" required />
                         <input className="input" autoComplete="email" aria-label="Email" id="email" type="email" placeholder="Email" required />
                         <input className="input" aria-label="Password" id="pass" type="password" placeholder="Password" required />
-                        <button className="submit btn">Join</button>
+                        <button className="submit btn" onSubmit={(e) => this.putUser(e)}>Join</button>
                     </form>
                 </section>
             </main>
@@ -22,4 +29,4 @@ class NewAccount extends Component {
     }
 }
 
-export default NewAccount
+export default NewAccount;
