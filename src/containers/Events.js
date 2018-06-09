2c7b98b5cf8ffc640548c61bcaa6e18c445b000d
Armando Jr <elfiservice@hotmail.com>
2018-06-09 10:08:31 -0300

    feat: provided list of events in Events compt

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -6,13 +6,28 @@ import { checkNewEvent } from '../server'
 
 
 class Events extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            listOfEvents: []
+        }
+    }
 
     componentDidMount() {
         let checkEvents = checkNewEvent(this.props.userStatus.uid)
-        checkEvents.on('value', function(snapshot) {
-            //updateStarCount(postElement, snapshot.val());
-            console.log(snapshot.val());
+        checkEvents.on('value', (snapshot) => {
 
+            if(snapshot.val() != null) {
+                const listOfEvents = snapshot.val()
+                let array = [];
+                for (var key in listOfEvents) {
+
+                    listOfEvents[key].id = key;
+                    array.push(listOfEvents[key])
+                  }
+
+                this.setState({ listOfEvents : array })
+            }
         });
     }
     
@@ -25,14 +40,16 @@ class Events extends Component {
         if(!userStatus){
             return <Redirect to="/login" />
         }
-console.log(userStatus);
+console.log(this.state.listOfEvents);
 
         return (
             <section className="events container">
                 <h2>Manage your Events Here</h2>
                 <p>Hi, {userStatus.displayName} <button className="btn" onClick={this.signOut}>Sign Out</button></p>
                 <Link to="/new-event" className="bkg-color-green rounded-btn" > &#9532; </Link> 
-
+                {this.state.listOfEvents.map((event, index) => (
+                    <li key={index}>{event.id}</li>
+                ))}
             </section>
         )
     }
