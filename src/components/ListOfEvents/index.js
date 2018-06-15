c5106a3613147657ef80f4acadaef3e895c59e89
Armando Jr <elfiservice@hotmail.com>
2018-06-15 07:38:04 -0300

    feat: provided a good format to Dates in ListOfEvents

--- src/components/ListOfEvents/index.js
+++ src/components/ListOfEvents/index.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import './ListOfEvents.css'
 
 class ListOfEvents extends Component {
+
+    formatDate(date) {
+        var d = new Date(date);
+        let day = d.getDate()
+        let month = d.getMonth()
+        let year = d.getFullYear()
+        let hours = d.getHours()
+        let min = d.getMinutes()
+        return `${day}/${month + 1}/${year} at ${(hours < 10 ? "0"+hours : hours)}:${(min < 10 ? "0"+min : min)}`
+    }
+
     render() {
         return (
             <section className="container">
@@ -16,7 +27,7 @@ class ListOfEvents extends Component {
                     {this.props.eventList.map((event, index) => (
                         <tr key={index}>
                             <td>{event.nameOfEvent}</td>
-                            <td>{event.startDateTime}</td>
+                            <td>{this.formatDate(event.startDateTime)}</td>
                         </tr>
                     ))}
                     </tbody>
