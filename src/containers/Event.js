21ae25ad1dcb07e8a92055569d9d63a8ade44c44
Armando Jr <elfiservice@hotmail.com>
2018-06-17 09:36:52 -0300

    feat: provided display all data of event

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { getEvent } from '../server'
+import '../containers/Event.css'
 
 class Event extends Component {
     constructor(props) {
@@ -38,10 +39,30 @@ class Event extends Component {
     }
 
     render() {
+        if (!this.props.userStatus) {
+            return <div>Loading...</div>
+          }
 
         return (
-            <section>
+            <section className="container event">
                 <h2>{this.state.event.nameOfEvent} Event Page</h2>
+                <div className="content">
+                    <p>Type of event <b>{this.state.event.typeEvents}</b></p>
+                    <p>Event host <b>{this.state.event.eventHost}</b></p>
+                    <p>Start at <b>{this.state.event.startDateTime}</b></p>
+                    <p>Until at <b>{this.state.event.endDateTime}</b></p>
+                    <p>Location <b>{this.state.event.location}</b></p>
+                    <p>Guest list 
+                        <div className="box-textarea">
+                            <b>{this.state.event.guestList}</b>
+                        </div>
+                    </p>
+                    <p>Message 
+                        <div className="box-textarea">
+                            <b>{this.state.event.message}</b>
+                        </div>
+                    </p>
+                </div>
             </section>
         )
     }
