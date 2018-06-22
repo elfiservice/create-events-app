af0bf8673415cc55012be6cef7d61999efaf6d67
Armando Jr <elfiservice@hotmail.com>
2018-06-22 08:52:16 -0300

    feat: added Modal comtp

---
+++ src/components/Modal.js/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Modal.css'
+
+class Modal extends Component {
+    render() {
+        return (
+            <div className="modal_window">
+                <article className="modal_content">
+                    teste AQUI!
+                </article>
+
+            </div>
+        )
+    }
+}
+
+export default Modal
