62b3ede861ad2e0a30805b413df2c037d8ebc09f
Armando Jr <elfiservice@hotmail.com>
2018-06-26 08:44:04 -0300

    feat: added some roles and aria in Event compt

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -46,25 +46,27 @@ class Event extends Component {
 
         return (
             <section className="container event">
-                <h2><i>{this.state.event.nameOfEvent}</i> Event Page</h2>
-                <div className="content">
-                    <p>Type of event <b>{this.state.event.typeEvents}</b></p>
-                    <p>Event host <b>{this.state.event.eventHost}</b></p>
-                    <p>Start at <b>{Helpers.formatDate(this.state.event.startDateTime)}</b></p>
-                    <p>Until at <b>{Helpers.formatDate(this.state.event.endDateTime)}</b></p>
-                    <p>Location <b>{this.state.event.location}</b></p>
-                    <div className="div-paragraph">Guest list 
+                <h2 tabIndex="1"><i>{this.state.event.nameOfEvent}</i> Event Page</h2>
+                <div className="content" tabIndex="2">
+                    <p>Type of event: <b>{this.state.event.typeEvents}</b></p>
+                    <p>Event host: <b>{this.state.event.eventHost}</b></p>
+                    <p>Start at: <b>{Helpers.formatDate(this.state.event.startDateTime)}</b></p>
+                    <p>Until at: <b>{Helpers.formatDate(this.state.event.endDateTime)}</b></p>
+                    <p>Location: <b>{this.state.event.location}</b></p>
+                    <div className="div-paragraph">Guest list: 
                         <div className="box-textarea">
                             <b>{this.state.event.guestList}</b>
                         </div>
                     </div>
-                    <div className="div-paragraph">Message 
+                    <div className="div-paragraph">Message: 
                         <div className="box-textarea">
                             <b>{this.state.event.message}</b>
                         </div>
                     </div>
                 </div>
-                <Link to="/events" className="bkg-color-red rounded-btn" > &#8617; </Link>
+                <Link to="/events" className="bkg-color-red rounded-btn"
+                    role="button"
+                    aria-label="Back to events list"> &#8617; </Link>
             </section>
         )
     }
