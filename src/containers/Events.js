66c94a1b4af1c226ddc114fc0509c41e47219afe
Armando Jr <elfiservice@hotmail.com>
2018-07-12 11:56:10 -0300

    feat: provided list of events via Redux

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -4,14 +4,10 @@ import { signOut, checkNewEvent } from '../server'
 import ListOfEvents from '../components/ListOfEvents'
 
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getList } from '../containers/eventsAcitons'
 
 class Events extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            listOfEvents: []
-        }
-    }
 
     componentDidMount() {
         checkNewEvent(this.props.userStatus.uid)
@@ -24,7 +20,8 @@ class Events extends Component {
                         array.push(listOfEvents[key])
                     }
                 }
-                this.setState({ listOfEvents : array })
+                // this.setState({ listOfEvents : array })
+                this.props.getList(array)
             });
     }
     
@@ -46,7 +43,7 @@ class Events extends Component {
             <section className="events container">
                 <h2 tabIndex="0">Manage your Events Here</h2>
                 <p>Hi, {userStatus.displayName} <button className="btn btn-singout" onClick={this.signOut} aria-label="Log out button"><i className="fas fa-sign-out-alt"></i></button></p>
-                <ListOfEvents eventList={this.state.listOfEvents} userStatus={userStatus} />
+                <ListOfEvents eventList={this.props.listOfEvents} userStatus={userStatus} />
                 <Link to="/new-event" 
                     className="bkg-color-green rounded-btn"
                     role="button"
@@ -56,5 +53,6 @@ class Events extends Component {
     }
 }
 
-const mapStateToProps = state => ({ userStatus: state.userStatus.userAuthenticated })
-export default connect(mapStateToProps)(Events)
+const mapStateToProps = state => ({ userStatus: state.userStatus.userAuthenticated, listOfEvents: state.eventsList.list })
+const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Events)
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import checkAuthReducer from '../containers/appReducer'
+import eventsList from '../containers/eventsReducer'
 
 const rootReducers = combineReducers({
-    userStatus: checkAuthReducer
+    userStatus: checkAuthReducer,
+    eventsList: eventsList
 })
 
 export default rootReducers
